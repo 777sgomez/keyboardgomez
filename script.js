d3b383f47c88e4da7d3e33cb65bed20692e29f93
Susan Gomez <susangomez@Susans-MBP.attlocal.net>
2023-07-12 13:47:18 -0400

    Added an EventListener with a keydown and event function to script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let randomLetter = letters[Math.floor(Math.random() * 26)];
 console.log(randomLetter);
+
+document.addEventListener('keydown', function(event) {
+
+})
