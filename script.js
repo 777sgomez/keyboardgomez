d951d70d62084a6f12768ed480d2888615259468
Susan Gomez <susangomez@Susans-MBP.attlocal.net>
2023-07-12 13:44:52 -0400

    Added a let randomLetter to equal a Math.floor to randomly choose a letter from the letters variable and added a console.log for it to script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let randomLetter = letters[Math.floor(Math.random() * 26)];
+console.log(randomLetter);
